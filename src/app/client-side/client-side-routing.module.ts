7caf6e8e03f2b90482283c39555d4fa61187fb43
fakhr-e-alam <fakhrealamtkoi777@gmail.com>
2024-08-22 17:49:41 +0500

    <addfooter >

--- src/app/client-side/client-side-routing.module.ts
+++ src/app/client-side/client-side-routing.module.ts
@@ -2,9 +2,9 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ContactToAdminComponent } from './components/contact-to-admin/contact-to-admin.component';
-
 import { LayoutComponent } from './components/layout/layout.component';
 import { MyblogsComponent } from './components/myblogs/myblogs.component';
+import { FooterComponent } from './components/footer/footer.component';
 const routes: Routes = [
 {
   path:'',
@@ -15,7 +15,7 @@ const routes: Routes = [
     { path: 'myblogs', component: MyblogsComponent },
     { path: 'contact-to-admin', component: ContactToAdminComponent },
     { path: '', redirectTo: 'home', pathMatch: 'full' }, // Default route
-
+    {path: 'footer', component: FooterComponent }
 
   ],
 } 
