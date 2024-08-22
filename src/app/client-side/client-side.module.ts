a37c220238a9cb7f79aa9838b2e45b3df93f87e2
mubashiriqball <mubashir6028@gmail.com>
2024-08-22 13:25:50 +0500

    new project

---
+++ src/app/client-side/client-side.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ClientSideRoutingModule } from './client-side-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ClientSideRoutingModule
+  ]
+})
+export class ClientSideModule { }
